fb09833473dbb384bdb1dcf97e598ecdcf7ab64a
Yehuda Katz <yehuda@ymkatz.net>
2013-09-09 22:10:55 -0400

    Added reminder to checkout submodules instead of generic 'cant include file' error

--- init.php
+++ init.php
@@ -1,5 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load Mustache for PHP
-include Kohana::find_file('vendor', 'mustache/src/Mustache/Autoloader');
+$mustache_src = Kohana::find_file('vendor', 'mustache/src/Mustache/Autoloader');
+if (empty($mustache_src))
+{
+	throw new Exception("Can not find Mustache source. Did you forget to initialize the submodule?");
+}
+include $mustache_src;
 Mustache_Autoloader::register();
